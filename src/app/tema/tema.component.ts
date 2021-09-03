f8b2f693fc7eac2cd7ec2ac4580bc5fc59380e59
janainarodrigues <janaina.rodrigues03@icloud.com>
2021-09-03 01:46:25 -0300

    Atualização componente tema e inclusão do post,get, put delete de tema I

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -8,9 +10,12 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
+  tema: Tema = new Tema()
+  listaTemas:Tema[]
 
   constructor(
-    private router:Router
+    private router:Router,
+    private temaService:TemaService
   ) { }
 
   ngOnInit(){
@@ -18,6 +23,20 @@ export class TemaComponent implements OnInit {
       /*alert('Sua seção expirou, faça o login novamente.')*/
       this.router.navigate(['/entrar'])
        }
+       this.findAllTema()
   }
 
+findAllTema(){
+  this.temaService.getAllTema().subscribe((resp:Tema[])=>{
+    this.listaTemas = resp
+  })
+}
+
+ cadastrar(){
+   this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+    this.tema = resp
+    alert('Tema cadastrado com sucecesso')
+    this.tema = new Tema()
+   })
+ }
 }
